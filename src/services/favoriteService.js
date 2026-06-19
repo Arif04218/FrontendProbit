8da718686c038e7df80b4d3782516353974c2003
Arif Rahman Hakim <burninglegion11@gmail.com>
2026-06-19 20:01:30 +0700

    fix api favorite

--- src/services/favoriteService.js
+++ src/services/favoriteService.js
@@ -3,22 +3,14 @@ import api from "./api";
 export const addFavoriteRecipe = (recipeId) =>
   api.post("/user/favorites", { recipe_id: recipeId });
 
-export const getFavoriteRecipes = () =>
-  api.get("/user/favorites");
-
-export const deleteFavoriteRecipe = (favoriteId) =>
-  api.delete(`/user/favorites/${favoriteId}`);
-
-
-// Favorite
-export const getFavorites = () =>
-  api.get("/user/favorites");
-
 export const addFavoriteBlog = (blogId) =>
   api.post("/user/favorites/blogs", { blog_id: blogId });
 
-export const deleteFavorite = (favoriteId) =>
-  api.delete(`/user/favorites/${favoriteId}`);
+export const getFavorites = () =>
+  api.get("/user/favorites");
 
-export const deleteFavoriteBlog = (favoriteId) =>
-  api.delete(`/user/favorites/blogs/${favoriteId}`);
+export const deleteFavorite = (id) =>
+  api.delete(`/user/favorites/${id}`);
+
+export const deleteFavoriteBlog = (id) =>
+  api.delete(`/user/favorites/blogs/${id}`);
